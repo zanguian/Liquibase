e22fe5e4dda45b75c12490b0ad034c5f64401958
Zachory <zachoryanguiano@gmail.com>
2022-06-16 21:57:41 -0600

    Initial commit

---
+++ src/main/java/com/example/mySpringBootApp/MySpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mySpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootAppApplication.class, args);
+	}
+
+}
